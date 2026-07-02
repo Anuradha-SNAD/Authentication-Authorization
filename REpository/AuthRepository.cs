a3bb479538cf098261d732f119a2711710c4fc97
Anuradha-SNAD <anuradha.avaduthwar@snaddevelopers.com>
2026-07-02 19:20:41 +0530

    Add files via upload

---
+++ REpository/AuthRepository.cs
@@ -0,0 +1,34 @@
+﻿using ProductManagement.Data;
+using ProductManagement.DTOs;
+using ProductManagement.Model;
+
+namespace ProductManagement.Repository
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly AppDbContext context;
+
+        public AuthRepository(AppDbContext context)
+        {
+            this.context = context;
+        }
+        public User Login(LoginRequestDTO dto)
+        {
+            return context.users.FirstOrDefault(x => x.Email == dto.Email && x.Password == dto.Password);
+        }
+
+        public void Register(UserRegistrationRequestDTO dto)
+        {
+            User u = new User()
+            {
+                UserName = dto.UserName,
+                Email = dto.Email,
+                Password = dto.Password,
+                Role = dto.Role,
+            };
+            context.users.Add(u);
+            context.SaveChanges();  
+        }
+    }
+    
+}
---
+++ REpository/IAuthRepository.cs
@@ -0,0 +1,11 @@
+﻿using ProductManagement.DTOs;
+using ProductManagement.Model;
+
+namespace ProductManagement.Repository
+{
+    public interface IAuthRepository
+    {
+        void Register(UserRegistrationRequestDTO dto);
+        User Login(LoginRequestDTO dto);
+    }
+}
