faf1eff02e0275568c6a7c0278f341519832e19c
Anuradha-SNAD <anuradha.avaduthwar@snaddevelopers.com>
2026-07-02 19:19:08 +0530

    Add files via upload

---
+++ Controller/AuthController.cs
@@ -0,0 +1,50 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ProductManagement.DTOs;
+using ProductManagement.Service;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace ProductManagement.Controllers.V3
+{
+    [ApiController]
+    [ApiVersion("3.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [SwaggerTag("Authentication Operations")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IProductService service;
+        private readonly IAuthService authService;
+        private readonly IJwtService jwtService;
+
+        public AuthController(IProductService service,IAuthService authService, IJwtService jwtService)
+        {
+            this.service = service;
+            this.authService = authService;
+            this.jwtService = jwtService;
+        }
+
+
+        [HttpPost("register")]
+        public IActionResult Register(UserRegistrationRequestDTO dto)
+        {
+            authService.Register(dto);
+
+            return Ok("User Registered Successfully");
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(LoginRequestDTO dto)
+        {
+            var user = authService.Login(dto);
+
+            var token = jwtService.GenerateToken(user);
+
+            return Ok(new
+            {
+                Token = token
+            });
+        }
+
+    }
+}
